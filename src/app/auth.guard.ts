c65a08f20b13ec2f9bc927e97429ea6c3ea46114
Pratik Prabhakarrao <pratikprabhakarraoatwork@gmail.com>
2024-03-14 21:21:20 +0530

    second Commit on 14

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn } from '@angular/router';
+import { SellerService } from './services/seller.service';
+import { inject } from '@angular/core';
+
+
+export const authGuard: CanActivateFn = (route, state) => {
+const seller = inject(SellerService)
+
+if(localStorage.getItem('seller')){
+  return true;
+}
+
+  return seller.isSellerLoggedIn;
+};
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { sSignUp } from 'src/dataType';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SellerService {
+  isSellerLoggedIn= new BehaviorSubject<boolean>(false);
+  constructor(private http: HttpClient, private route:Router) {}
 
-  constructor(private http:HttpClient) { }
+  sellerSignUp(data: sSignUp) {
+    this.http
+      .post('http://localhost:3000/seller', data, { observe: 'response' })
+      .subscribe((result) => {
+        this.isSellerLoggedIn.next(true);
+        localStorage.setItem('seller',JSON.stringify(result.body))
+        this.route.navigate(['seller-home'])
+      });
 
-  sellerSignUp(data:sSignUp){
-    return this.http.post("http://localhost:3000/seller", data)
-    
-    
   }
+
+  reloadSeller(){
+    if(localStorage.getItem('seller')){
+      this.isSellerLoggedIn.next(true);
+      this.route.navigate(['seller-home'])
+    }
+  }
+
 }
